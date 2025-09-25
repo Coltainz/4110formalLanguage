a76203ea5cddc9c19ecfb155ba2245ec3691b706
ColtonAnderson <Coltainzzzz@gmail.com>
2025-09-25 15:39:00 -0600

    think task1 is done, not entirely sure though

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 print("Hello, formal language project!")
 
 def is_in_language (s):
-    if not s:
+    if len(s) == 0:
         return False
     
+    for ch in s:
+        if ch not in ("a", "b"):
+            return False
+    
+    i = 0
+    n = len(s)
+    while i < n and s[i] == 'a':
+        i+=1
+    
+    count_a = i
+
+    if count_a == 0:
+        return False
+    
+    while i < n and s[i] == 'b':
+        i+=1
+
+    count_b = n - count_a
+
+    if i != n:
+        return False
+    
+    if count_b != count_a:
+        return False
+    
+    return True
